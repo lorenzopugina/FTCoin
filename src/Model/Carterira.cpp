9c3a9b6204f02a19b0f809933d651f3094989334
marimuh <mariacsmuharem@gmail.com>
2025-06-07 13:40:03 -0300

    Update Carterira.cpp

---
+++ src/Model/Carterira.cpp
@@ -0,0 +1,60 @@
+#include "Carteira.hpp"
+#include <stdexcept> //para tratar exceções
+
+using namespace std;
+//construtor
+Carteira::Carteira() : id(0), nomeTitular(""), corretora("") {}
+//construtor parametizado
+Carteira::Carteira(int id, const string& titular, const string& corretora) 
+    : id(id), nomeTitular(titular), corretora(corretora) 
+{
+    if (!estaValida()) 
+    {//verifica se há um nome para titular e nome para corretora, ou sej, a se estão vazios ou nao
+        throw invalid_argument("Dados inválidos para criação da carteira");
+    }
+}
+//destrutor
+Carteira::~Carteira() {}
+
+// Getters
+int Carteira::getId() const {
+    return id;
+}
+
+string Carteira::getNomeTitular() const {
+    return nomeTitular;
+}
+
+string Carteira::getCorretora() const {
+    return corretora;
+}
+
+// Setters
+void Carteira::setId(int id) 
+{  
+    this->id = id;
+}
+
+void Carteira::setNomeTitular(const string& newTitular) 
+{
+    if (newTitular.empty()) 
+    {//se n houver um nome do titular, esta errado
+        throw invalid_argument("Nome do titular não pode ser vazio");
+    }
+    nomeTitular = newTitular;
+}
+
+void Carteira::setCorretora(const string& newCorretora) 
+{
+    if (newCorretora.empty()) 
+    {//se n houver um nome da corretora, esta errado
+        throw invalid_argument("Corretora não pode ser vazia");
+    }
+    corretora = newCorretora;
+}
+
+// Validação do model
+bool Carteira::estaValida() const 
+{
+    return (id > 0 && !nomeTitular.empty() && !corretora.empty());
+}
