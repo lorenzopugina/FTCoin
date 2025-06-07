3493892f9c5930b523b8f5839616ee9f1214199a
marimuh <mariacsmuharem@gmail.com>
2025-06-07 01:52:22 -0300

    Update Carteira.h

--- src/Model/Carteira.h
+++ src/Model/Carteira.h
@@ -1,14 +1,36 @@
-#ifndef CARTERIA_H_
-#define CARTEIRA_H_
+#ifndef CARTEIRA_HPP
+#define CARTEIRA_HPP
 
-class Carteira{
-  private:
-   
-  
-  public:
+#include <string>
+
+using namespace std;
+
+class Carteira 
+{
+private:
+    int id;
+    string nomeTitular;
+    string corretora;
+public:
+    //construtores
     Carteira();
+    Carteira(int id, const string& titular, const string& corretora);
+    
+    //destrutor
     ~Carteira();
 
+//getters
+int getId() const;
+string getNomeTitular() const;
+string getCorretora() const;
+
+//setters
+void setId(int id);
+void setNomeTitular(const string& newtitular);
+void setCorretora(const string& newcorretora);
+
+ // Validação do model
+bool estaValida() const;
 };
 
 #endif
